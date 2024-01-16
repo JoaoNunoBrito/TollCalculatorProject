bcb8a41c12883f677e03977494c98d413e0322e0
JoaoNunoBrito <jnabrito@gmail.com>
2024-01-16 01:44:34 +0100

    Init

---
+++ src/TollCalculator/Entities/Enums/VehicleEnums.cs
@@ -0,0 +1,30 @@
+﻿namespace TollFeeCalculator.Entities.Enums
+{
+    public class VehicleEnums
+    {
+        public enum VehicleTypeEnum
+        {
+            Car = 0,
+            Truck = 1,
+            SUV = 2,
+            Motorbike = 3,
+            Tractor = 4,
+            Emergency = 5,
+            Diplomat = 6,
+            Foreign = 7,
+            Military = 8,
+            Others = 9
+        }
+
+        //Subset of VehicleTypeEnum. Ensure that the Vehicles' int below matches the int in VehicleTypeEnum
+        public enum TollFreeVehicles
+        {
+            Motorbike = 3,
+            Tractor = 4,
+            Emergency = 5,
+            Diplomat = 6,
+            Foreign = 7,
+            Military = 8
+        }
+    }
+}
---
+++ src/TollCalculator/Entities/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿using static TollFeeCalculator.Entities.Enums.VehicleEnums;
+
+namespace TollFeeCalculator.Vehicles
+{
+    public class Vehicle
+    {
+        public VehicleTypeEnum VehicleType { get; set; }
+        public VehicleTypeEnum GetVehicleType()
+        {
+            return VehicleType;
+        }
+    }
+}
